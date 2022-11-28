afdb14b6b19270bbbfbd28d08501bb4948be48c2
dai-1103 <bosatsunumnum@gmail.com>
2022-11-29 05:09:39 +0900

    クエスト開始時のバリデーションを作成

--- src/main/java/net/kunmc/lab/tributeplugin/command/RequestStart.java
+++ src/main/java/net/kunmc/lab/tributeplugin/command/RequestStart.java
@@ -1,6 +1,7 @@
 package net.kunmc.lab.tributeplugin.command;
 
 import net.kunmc.lab.commandlib.Command;
+import net.kunmc.lab.tributeplugin.Store;
 import net.kunmc.lab.tributeplugin.request.RequestManager;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -16,6 +17,10 @@ public class RequestStart extends Command {
           .integerArgument("timelimit")
           .execute(
               commandContext -> {
+                if (Store.config.requester.toPlayer() == null) {
+                  commandContext.sendFailure("requesterが設定されていないかサーバーに存在しません");
+                  return;
+                }
                 Material target = ((ItemStack) commandContext.getParsedArg("item")).getType();
                 int amount = (int) commandContext.getParsedArg("amount");
                 int timeLimit = (int) commandContext.getParsedArg("timelimit");
